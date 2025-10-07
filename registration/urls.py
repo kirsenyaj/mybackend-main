1be290977c073b2588607fe3ec08318601911ab6
kirsenyaj <kirsenyajvillanueva@gmail.com>
2025-10-07 12:42:55 +0800

    lab act 03

---
+++ registration/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('api/register/', views.register_user, name='register_api'),
+    path('api/users/', views.list_user, name='list_user'),
+]
+
