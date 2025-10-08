751b5c6bbe7ada0fd87eb9302bfc7bae7dffe5df
kirsenyaj <kirsenyajvillanueva@gmail.com>
2025-10-08 12:23:31 +0800

    updated views,settings,urls.py

--- registration/urls.py
+++ registration/urls.py
@@ -4,5 +4,6 @@ from . import views
 urlpatterns = [
     path('api/register/', views.register_user, name='register_api'),
     path('api/users/', views.list_user, name='list_user'),
+    path('api/users/<int:pk>/', views.user_detail, name="user_detail"),
 ]
 
