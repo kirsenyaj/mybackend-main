21298d358e09ec0de2cef87ee2b8f8259c31a47f
kirsenyaj <kirsenyajvillanueva@gmail.com>
2025-11-05 10:44:38 +0800

    configured settings.py again for deploying

--- MyProject/settings.py
+++ MyProject/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-vvsy!n%)+9iit&ms$addc01r%qx%z!+r=o_%!2o6x3c3r*)6-e
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['http://127.0.0.1:8000/','127.0.0.1:8000','127.0.0.1','http://192.168.30.230:8000/','192.168.30.230:8000','192.168.30.230', 'https://peitel-backend.onrender.com', 'peitel-backend.onrender.com']
+ALLOWED_HOSTS = ['http://127.0.0.1:8000/','127.0.0.1:8000','127.0.0.1','http://192.168.30.230:8000/','192.168.30.230:8000','192.168.30.230', 'https://peitel-mybackend.onrender.com', 'peitel-mybackend.onrender.com', '.onrender.com']
 
 CSRF_TRUSTED_ORIGINS = [
     "https://peitelbackend.onrender.com",
